2a2044161b22082255342c233b8baa803fd31926
henriquewilson1 <me@henriquewilson.com.br>
2015-07-14 18:13:59 -0300

    first commit.

---
+++ Example/Pods/Target Support Files/Pods-HWSomething_Tests-HWSomething/Pods-HWSomething_Tests-HWSomething-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HWSomething_Tests_HWSomething : NSObject
+@end
+@implementation PodsDummy_Pods_HWSomething_Tests_HWSomething
+@end
---
+++ Example/Pods/Target Support Files/Pods-HWSomething_Tests/Pods-HWSomething_Tests-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HWSomething
+#define COCOAPODS_POD_AVAILABLE_HWSomething
+#define COCOAPODS_VERSION_MAJOR_HWSomething 0
+#define COCOAPODS_VERSION_MINOR_HWSomething 1
+#define COCOAPODS_VERSION_PATCH_HWSomething 0
+
